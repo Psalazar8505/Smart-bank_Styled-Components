fbc8b9e43bcf6fdc27f63ab440acf127eb3dbfbb
Psalazar8505 <psalazar.ol71@gmail.com>
2023-05-17 11:03:44 -0600

    terminando la clase 4 'nuevo item'

---
+++ src/ImageFilter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import alimentacion from "./assets/images/alimentacion.svg";
+import salud from "./assets/images/salud.svg";
+import otros from "./assets/images/otros.svg";
+import transporte from "./assets/images/transporte.svg";
+import utilidades from "./assets/images/utilidades.svg";
+import { Icono } from "./Components/UI";
+
+export default (type) =>{
+    const Images = {
+        Restaurante: <Icono src={alimentacion} alt="Restaurante" />,
+        Salud: <Icono src={salud} alt="Salud" />,
+        // Otros: <Icono src={otros} alt="Otros"/>,
+        Transporte: <Icono src={transporte} alt="Transporte" />,
+        Utilidades: <Icono src={utilidades} alt="Utilidades" />,
+        default : <Icono src={otros} alt="Otros" />
+    };
+
+    return Images [type] || Images ["default"];
+}
