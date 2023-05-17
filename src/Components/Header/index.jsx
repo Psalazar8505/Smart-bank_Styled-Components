5a239544489a6dd31b302e4a9af5fdb41593e3d1
Psalazar8505 <psalazar.ol71@gmail.com>
2023-05-16 18:52:09 -0600

    terminando la segunda clase 'topicos basicos'

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,19 +1,50 @@
 import React from "react";
+import { styled } from "styled-components";
 import logo from "../../assets/images/logo.svg";
+import { colorPrimario } from "../UI/variales";
+
+console.log(colorPrimario)
+
+const StyleHeader = styled.nav`
+  background-color: ${colorPrimario};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`
+
+const BtnHeader = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+  color: ${ ({primary}) => primary ? colorPrimario : "white" };
+  background: ${ (props) => props.primary ? "white" : "transparent" };
+`
+
+
 
 const Header = () => {
   return (
-    <div className="header">
-      <img className="imagen-logo" src={logo} alt="Logo Smart Bank" />
+    <StyleHeader>
+      <Logo src={logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnHeader primary href="https://google.com">
           Ayuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnHeader>
+        <BtnHeader href="https://google.com">
           Salir
-        </a>
+        </BtnHeader>
       </div>
-    </div>
+    </StyleHeader>
   );
 };
 
